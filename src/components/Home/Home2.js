7dfa64f2ade4956269f1991e360c285f265403bf
dheeksha178811 <dheeksha178811@gmail.com>
2026-01-29 14:37:08 +0530

    Update portfolio with personal image and remove avatar - replace home-main.svg with dheeksha image.png and remove top avatar

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -1,14 +1,13 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import myImg from "../../Assets/avatar.svg";
-import Tilt from "react-parallax-tilt";
+
 
 function Home2() {
   return (
     <Container fluid className="home-about-section" id="about">
       <Container>
         <Row>
-          <Col md={8} className="home-about-description">
+          <Col className="home-about-description">
             <h1 style={{ fontSize: "2.6em" }}>
               LET ME <span className="purple"> INTRODUCE </span> MYSELF
             </h1>
@@ -47,11 +46,7 @@ function Home2() {
               </i>
             </p>
           </Col>
-          <Col md={4} className="myAvtar">
-            <Tilt>
-              <img src={myImg} className="img-fluid" alt="avatar" />
-            </Tilt>
-          </Col>
+
         </Row>
       </Container>
     </Container>
